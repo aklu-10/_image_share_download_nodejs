3c5d8bf32327000e64e362677a6c3467949166db
Akash <you@example.com>
2023-01-26 21:17:08 +0530

    v1.0.0

---
+++ index.js
@@ -0,0 +1,42 @@
+const express=require("express");
+const app=express();
+const multer=require("multer");
+var path = require('path');
+var fs = require('fs');
+
+app.set('view engine','ejs');
+app.use(express.static(__dirname+"/public"))
+
+app.get("/",(req,res)=>
+{
+    res.render("index",{allFiles:''});
+})
+
+
+var directoryPath = path.join(__dirname, '/public/uploads');
+
+const upload=multer({
+    storage:multer.diskStorage({
+        destination:(req,file,cb)=>
+        {
+            cb(null,"public/uploads");
+        },
+        filename:(req,file,cb)=>
+        {
+            cb(null,file.fieldname+"-"+Date.now()+'.jpg');
+        }
+    })
+}).array("mypic");
+
+app.post("/uploadPicture",upload,(req,res)=>
+{
+    fs.readdir(directoryPath, function (err, files) {
+        if (err) {
+            return console.log('Unable to scan directory: ' + err);
+        }
+        res.render("index",{allFiles:files});
+    });
+
+})
+
+app.listen(3000);
